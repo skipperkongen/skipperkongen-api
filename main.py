90afd159c35cb2618af38aa9670b5ec67900dff7
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2021-09-12 16:36:33 +0200

    redis counter 2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import redis
 
 app = FastAPI()
 
@@ -8,6 +9,9 @@ def home():
 
 @app.get("/redis-counter/")
 def redis_counter():
+    """
+    https://devcenter.heroku.com/articles/heroku-redis#connecting-in-python
+    """
     r = redis.from_url(os.environ.get("REDIS_URL"))
     value = r.incr('redis_counter')
     return {"redis_counter": int(value)}
